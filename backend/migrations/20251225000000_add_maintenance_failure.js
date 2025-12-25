363e94660624836fb8783b7eb735d61e1ea8b584
GitHub <noreply@github.com>
2025-12-25 04:02:08 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- backend/migrations/20251225000000_add_maintenance_failure.js
+++ backend/migrations/20251225000000_add_maintenance_failure.js
@@ -3,17 +3,17 @@ import { migrate as logger } from "../logger.js";
 const migrateName = "add_maintenance_failure";
 
 export const up = (knex) => {
-    logger.info(`[${migrateName}] Migrating Up...`);
+	logger.info(`[${migrateName}] Migrating Up...`);
 
-    return knex.schema.table("proxy_host", (table) => {
-        table.integer("maintenance_on_failure").notNull().unsigned().defaultTo(0);
-    });
+	return knex.schema.table("proxy_host", (table) => {
+		table.integer("maintenance_on_failure").notNull().unsigned().defaultTo(0);
+	});
 };
 
 export const down = (knex) => {
-    logger.info(`[${migrateName}] Migrating Down...`);
+	logger.info(`[${migrateName}] Migrating Down...`);
 
-    return knex.schema.table("proxy_host", (table) => {
-        table.dropColumn("maintenance_on_failure");
-    });
+	return knex.schema.table("proxy_host", (table) => {
+		table.dropColumn("maintenance_on_failure");
+	});
 };
