5973a4951e3e5e3dcfeb1990e73f890146d66503
shedowe <shedowe@eBlocker-test.localdomain>
2025-12-27 03:17:02 +0100

    chore: remove analytics debug logging

--- frontend/src/pages/Analytics/index.tsx
+++ frontend/src/pages/Analytics/index.tsx
@@ -27,7 +27,6 @@ const Analytics = () => {
 			try {
 				// Fetch Summary
 				const summaryData = await getAnalyticsSummary();
-				console.log("FRONTEND SUMMARY DEBUG:", summaryData); // DEBUG
 				setSummary(summaryData);
 
 				// Fetch Series
