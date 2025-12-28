3cb323a58ae3cf18084f287001c9e9f09a0c6428
shedowe19 <76975113+shedowe19@users.noreply.github.com>
2025-12-28 23:50:49 +0100

    style: increase width of analytics top lists

--- frontend/src/pages/Analytics/index.tsx
+++ frontend/src/pages/Analytics/index.tsx
@@ -323,7 +323,7 @@ const Analytics = () => {
 			</div>
 
 			{/* Top Lists Row 1 */}
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 				<Card>
 					<CardHeader>
 						<CardTitle>Top IPs</CardTitle>
