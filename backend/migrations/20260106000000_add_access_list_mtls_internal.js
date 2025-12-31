d3709821cf203210d0fdf5a5a4df0615587810e5
GitHub <noreply@github.com>
2025-12-31 15:19:22 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- backend/migrations/20260106000000_add_access_list_mtls_internal.js
+++ backend/migrations/20260106000000_add_access_list_mtls_internal.js
@@ -11,14 +11,15 @@ const migrateName = "add_access_list_mtls_internal";
  * @returns {Promise}
  */
 const up = (knex) => {
-    logger.info(`[${migrateName}] Migrating Up...`);
+	logger.info(`[${migrateName}] Migrating Up...`);
 
-    return knex.schema.table("access_list", (table) => {
-        table.integer("mtls_use_internal").notNullable().defaultTo(0).unsigned();
-    })
-        .then(() => {
-            logger.info(`[${migrateName}] access_list Table updated`);
-        });
+	return knex.schema
+		.table("access_list", (table) => {
+			table.integer("mtls_use_internal").notNullable().defaultTo(0).unsigned();
+		})
+		.then(() => {
+			logger.info(`[${migrateName}] access_list Table updated`);
+		});
 };
 
 /**
@@ -28,14 +29,15 @@ const up = (knex) => {
  * @returns {Promise}
  */
 const down = (knex) => {
-    logger.info(`[${migrateName}] Migrating Down...`);
+	logger.info(`[${migrateName}] Migrating Down...`);
 
-    return knex.schema.table("access_list", (table) => {
-        table.dropColumn("mtls_use_internal");
-    })
-        .then(() => {
-            logger.info(`[${migrateName}] access_list Table updated`);
-        });
+	return knex.schema
+		.table("access_list", (table) => {
+			table.dropColumn("mtls_use_internal");
+		})
+		.then(() => {
+			logger.info(`[${migrateName}] access_list Table updated`);
+		});
 };
 
 export { up, down };
