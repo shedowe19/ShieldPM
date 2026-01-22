81f1e31370af1ab5d97392c8fbf81f59786e9d6e
GitHub <noreply@github.com>
2026-01-22 06:00:37 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- backend/migrations/20260122200000_add_terminal_host.js
+++ backend/migrations/20260122200000_add_terminal_host.js
@@ -11,29 +11,29 @@ const migrateName = "add_terminal_host";
  * @returns {Promise}
  */
 const up = (knex) => {
-    logger.info(`[${migrateName}] Migrating Up...`);
+	logger.info(`[${migrateName}] Migrating Up...`);
 
-    return knex.schema
-        .createTable("terminal_host", (table) => {
-            table.increments("id").primary();
-            table.dateTime("created_on").notNullable();
-            table.dateTime("modified_on").notNullable();
-            table.integer("owner_user_id").unsigned().notNullable().references("id").inTable("user");
-            table.integer("enabled").notNullable().defaultTo(1);
-            table.string("type").notNullable().defaultTo("ssh"); // ssh, telnet, etc.
-            table.string("name").notNullable();
-            table.string("host").notNullable();
-            table.integer("port").notNullable().defaultTo(22);
-            table.string("auth_type").notNullable().defaultTo("password"); // password, key
-            table.string("username").notNullable();
-            table.string("password"); // Encrypted
-            table.string("private_key"); // Encrypted
-            table.json("meta").notNullable();
-            table.integer("is_deleted").notNullable().defaultTo(0);
-        })
-        .then(() => {
-            logger.info(`[${migrateName}] terminal_host Table created`);
-        });
+	return knex.schema
+		.createTable("terminal_host", (table) => {
+			table.increments("id").primary();
+			table.dateTime("created_on").notNullable();
+			table.dateTime("modified_on").notNullable();
+			table.integer("owner_user_id").unsigned().notNullable().references("id").inTable("user");
+			table.integer("enabled").notNullable().defaultTo(1);
+			table.string("type").notNullable().defaultTo("ssh"); // ssh, telnet, etc.
+			table.string("name").notNullable();
+			table.string("host").notNullable();
+			table.integer("port").notNullable().defaultTo(22);
+			table.string("auth_type").notNullable().defaultTo("password"); // password, key
+			table.string("username").notNullable();
+			table.string("password"); // Encrypted
+			table.string("private_key"); // Encrypted
+			table.json("meta").notNullable();
+			table.integer("is_deleted").notNullable().defaultTo(0);
+		})
+		.then(() => {
+			logger.info(`[${migrateName}] terminal_host Table created`);
+		});
 };
 
 /**
@@ -43,8 +43,8 @@ const up = (knex) => {
  * @returns {Promise}
  */
 const down = (knex) => {
-    logger.info(`[${migrateName}] Migrating Down...`);
-    return knex.schema.dropTable("terminal_host");
+	logger.info(`[${migrateName}] Migrating Down...`);
+	return knex.schema.dropTable("terminal_host");
 };
 
 export { up, down };
