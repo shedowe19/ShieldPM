fb616d74063231166f883022cd785ba7d7f276cb
GitHub <noreply@github.com>
2026-01-23 04:45:02 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- backend/migrations/20260123000000_add_user_permission_analytics.js
+++ backend/migrations/20260123000000_add_user_permission_analytics.js
@@ -9,27 +9,27 @@ const migrateName = "add_user_permission_analytics";
  * @returns {Promise}
  */
 const up = async (knex) => {
-    logger.info(`[${migrateName}] Migrating Up...`);
+	logger.info(`[${migrateName}] Migrating Up...`);
 
-    // Add cloudflared_tunnels column if it doesn't exist
-    const hasCloudflared = await knex.schema.hasColumn("user_permission", "cloudflared_tunnels");
-    if (!hasCloudflared) {
-        await knex.schema.alterTable("user_permission", (table) => {
-            table.string("cloudflared_tunnels").notNull().defaultTo("manage");
-        });
-        logger.info(`[${migrateName}] Added cloudflared_tunnels column`);
-    }
+	// Add cloudflared_tunnels column if it doesn't exist
+	const hasCloudflared = await knex.schema.hasColumn("user_permission", "cloudflared_tunnels");
+	if (!hasCloudflared) {
+		await knex.schema.alterTable("user_permission", (table) => {
+			table.string("cloudflared_tunnels").notNull().defaultTo("manage");
+		});
+		logger.info(`[${migrateName}] Added cloudflared_tunnels column`);
+	}
 
-    // Add analytics column if it doesn't exist
-    const hasAnalytics = await knex.schema.hasColumn("user_permission", "analytics");
-    if (!hasAnalytics) {
-        await knex.schema.alterTable("user_permission", (table) => {
-            table.string("analytics").notNull().defaultTo("view");
-        });
-        logger.info(`[${migrateName}] Added analytics column`);
-    }
+	// Add analytics column if it doesn't exist
+	const hasAnalytics = await knex.schema.hasColumn("user_permission", "analytics");
+	if (!hasAnalytics) {
+		await knex.schema.alterTable("user_permission", (table) => {
+			table.string("analytics").notNull().defaultTo("view");
+		});
+		logger.info(`[${migrateName}] Added analytics column`);
+	}
 
-    logger.info(`[${migrateName}] Migration complete`);
+	logger.info(`[${migrateName}] Migration complete`);
 };
 
 /**
@@ -39,21 +39,21 @@ const up = async (knex) => {
  * @returns {Promise}
  */
 const down = async (knex) => {
-    logger.info(`[${migrateName}] Migrating Down...`);
+	logger.info(`[${migrateName}] Migrating Down...`);
 
-    const hasCloudflared = await knex.schema.hasColumn("user_permission", "cloudflared_tunnels");
-    if (hasCloudflared) {
-        await knex.schema.alterTable("user_permission", (table) => {
-            table.dropColumn("cloudflared_tunnels");
-        });
-    }
+	const hasCloudflared = await knex.schema.hasColumn("user_permission", "cloudflared_tunnels");
+	if (hasCloudflared) {
+		await knex.schema.alterTable("user_permission", (table) => {
+			table.dropColumn("cloudflared_tunnels");
+		});
+	}
 
-    const hasAnalytics = await knex.schema.hasColumn("user_permission", "analytics");
-    if (hasAnalytics) {
-        await knex.schema.alterTable("user_permission", (table) => {
-            table.dropColumn("analytics");
-        });
-    }
+	const hasAnalytics = await knex.schema.hasColumn("user_permission", "analytics");
+	if (hasAnalytics) {
+		await knex.schema.alterTable("user_permission", (table) => {
+			table.dropColumn("analytics");
+		});
+	}
 };
 
 export { up, down };
