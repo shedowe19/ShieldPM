423fecce27890bbf0d953f48f16f39fc89505a3f
shedowe19 <lieske19372@gmail.com>
2026-03-20 12:24:59 +0100

    refactor: lazy load heavy modal entrypoints

--- frontend/src/modals/index.ts
+++ frontend/src/modals/index.ts
@@ -1,19 +1,51 @@
-export * from "./AccessListModal";
 export * from "./ChangePasswordModal";
 export * from "./CustomCertificateModal";
 export * from "./DashboardNoteModal";
-export * from "./DdnsProviderModal";
-export * from "./DeadHostModal";
 export * from "./DeleteConfirmModal";
 export * from "./DNSCertificateModal";
 export * from "./EventDetailsModal";
 export * from "./HelpModal";
 export * from "./HTTPCertificateModal";
 export * from "./InternalCertificateModal";
-export * from "./PermissionsModal";
-export * from "./ProxyHostModal";
-export * from "./RedirectionHostModal";
 export * from "./RenewCertificateModal";
 export * from "./SetPasswordModal";
-export * from "./StreamModal";
-export * from "./UserModal";
+
+export const showAccessListModal = async (...args: Parameters<typeof import("./AccessListModal")["showAccessListModal"]>) => {
+	const module = await import("./AccessListModal");
+	return module.showAccessListModal(...args);
+};
+
+export const showDdnsProviderModal = async (...args: Parameters<typeof import("./DdnsProviderModal")["showDdnsProviderModal"]>) => {
+	const module = await import("./DdnsProviderModal");
+	return module.showDdnsProviderModal(...args);
+};
+
+export const showDeadHostModal = async (...args: Parameters<typeof import("./DeadHostModal")["showDeadHostModal"]>) => {
+	const module = await import("./DeadHostModal");
+	return module.showDeadHostModal(...args);
+};
+
+export const showPermissionsModal = async (...args: Parameters<typeof import("./PermissionsModal")["showPermissionsModal"]>) => {
+	const module = await import("./PermissionsModal");
+	return module.showPermissionsModal(...args);
+};
+
+export const showProxyHostModal = async (...args: Parameters<typeof import("./ProxyHostModal")["showProxyHostModal"]>) => {
+	const module = await import("./ProxyHostModal");
+	return module.showProxyHostModal(...args);
+};
+
+export const showRedirectionHostModal = async (...args: Parameters<typeof import("./RedirectionHostModal")["showRedirectionHostModal"]>) => {
+	const module = await import("./RedirectionHostModal");
+	return module.showRedirectionHostModal(...args);
+};
+
+export const showStreamModal = async (...args: Parameters<typeof import("./StreamModal")["showStreamModal"]>) => {
+	const module = await import("./StreamModal");
+	return module.showStreamModal(...args);
+};
+
+export const showUserModal = async (...args: Parameters<typeof import("./UserModal")["showUserModal"]>) => {
+	const module = await import("./UserModal");
+	return module.showUserModal(...args);
+};
