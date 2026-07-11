e3a11b4660613cfcc077e4b0f913f74aadc1910b
Shedowe <shedowe@users.noreply.github.com>
2026-07-11 20:41:47 +0200

    [REFACTOR] Extract ProxyHost form defaults

--- frontend/src/modals/ProxyHostModal.tsx
+++ frontend/src/modals/ProxyHostModal.tsx
@@ -50,6 +50,7 @@ import {
 	TERMINAL_AUTH_TYPE,
 	TIME_UNIT,
 } from "src/types/enums";
+import { createProxyHostInitialValues, type ProxyHostFormValues } from "./ProxyHostModalFormValues";
 
 const DEFAULT_ANUBIS_RULES = [
 	{
@@ -75,16 +76,6 @@ interface Props extends InnerModalProps {
 	id: number | "new";
 	visible: boolean;
 	remove: () => void;
-}
-
-interface ProxyHostFormValues extends Omit<Partial<ProxyHost>, "advLimitReqRate" | "advLimitReqBurst"> {
-	advLimitReqRate?: number | string;
-	advLimitReqBurst?: number | string;
-	crowdsecEnabled?: boolean;
-	anubisEnabled?: boolean;
-	anubisRules?: ProxyHost["anubisRules"];
-	gitCredentials?: string;
-	php_override_ini?: string;
 }
 
 const ProxyHostModal = EasyModal.create(({ id, visible, remove }: Props) => {
@@ -154,80 +145,7 @@ const ProxyHostModal = EasyModal.create(({ id, visible, remove }: Props) => {
 						</div>
 					))}
 				{!isLoading && !userIsLoading && data && currentUser && (
-					<Formik
-						initialValues={
-							{
-								// Details tab
-								domainNames: data?.domainNames || [],
-								forwardScheme: data?.forwardScheme || FORWARD_SCHEME.HTTP,
-								forwardHost: data?.forwardHost || "",
-								forwardPort: data?.forwardPort || undefined,
-								indexFile: data?.indexFile || "", // Add indexFile to initialValues
-								// Terminal Fields
-								terminalHost: data?.terminalHost || "",
-								terminalPort: data?.terminalPort || 22,
-								terminalUsername: data?.terminalUsername || "",
-								terminalAuthType: data?.terminalAuthType || TERMINAL_AUTH_TYPE.PASSWORD,
-								terminalPassword: data?.terminalPassword || "",
-								terminalPrivateKey: data?.terminalPrivateKey || "",
-
-								accessListId: data?.accessListId || 0,
-								cachingEnabled: data?.cachingEnabled || false,
-								disableBuffering: data?.disableBuffering || false,
-								blockExploits: data?.blockExploits || false,
-								allowWebsocketUpgrade: data?.allowWebsocketUpgrade || false,
-								maintenanceOnFailure: data?.maintenanceOnFailure || false,
-								// PHP hosting (for scheme=path)
-								phpEnabled: data?.phpEnabled || false,
-								phpVersion: data?.phpVersion || PHP_VERSION.PHP83,
-								// Locations tab
-								locations: data?.locations || [],
-								// SSL tab
-								certificateId: data?.certificateId || 0,
-								sslForced: data?.sslForced || false,
-								http2Support: data?.http2Support || false,
-								hstsEnabled: data?.hstsEnabled || false,
-								hstsSubdomains: data?.hstsSubdomains || false,
-								// Advanced tab
-								advancedConfig: data?.advancedConfig || "",
-								bandwidthLimit: data?.bandwidthLimit || "",
-								turboLoader: data?.turboLoader || false,
-								advLimitReqRate: data?.advLimitReqRate || undefined,
-								advLimitReqUnit: data?.advLimitReqUnit || TIME_UNIT.SECONDS,
-								advLimitReqBurst: data?.advLimitReqBurst || undefined,
-								forwardQuery: data?.forwardQuery || "",
-								maintenanceActive: data?.maintenanceActive || false,
-								// datetime-local requires format: YYYY-MM-DDTHH:mm:ss (no timezone)
-								// API returns ISO format with 'Z' suffix, so we strip it
-								maintenanceStart: data?.maintenanceStart
-									? data.maintenanceStart.replace("Z", "").split(".")[0]
-									: "",
-								maintenanceEnd: data?.maintenanceEnd
-									? data.maintenanceEnd.replace("Z", "").split(".")[0]
-									: "",
-								maintenanceReason: data?.maintenanceReason || "",
-								// Git Sync
-								gitRepoUrl: data?.gitRepoUrl || "",
-								gitBranch: data?.gitBranch || "main",
-								gitSyncEnabled: data?.gitSyncEnabled || false,
-								gitPollInterval: data?.gitPollInterval || 60,
-								gitPollUnit: data?.gitPollUnit || TIME_UNIT.MINUTES,
-								gitCredentials: "", // Do not fill credentials for security
-								// Service Icon
-								iconType: data?.iconType || ICON_TYPE.AUTO,
-								iconUrl: data?.iconUrl || "",
-								// CrowdSec
-								crowdsecEnabled: data?.securityCrowdsec || false,
-								// Anubis
-								anubisEnabled: data?.anubisEnabled || false,
-								anubisRules: data?.anubisRules || [],
-								// Note
-								note: data?.note || "",
-							} as ProxyHostFormValues
-						}
-						enableReinitialize
-						onSubmit={onSubmit}
-					>
+					<Formik initialValues={createProxyHostInitialValues(data)} enableReinitialize onSubmit={onSubmit}>
 						{() => (
 							<Form className="flex flex-col h-full overflow-hidden">
 								<DialogHeader className="px-6 py-4 border-b">
---
+++ frontend/src/modals/ProxyHostModalFormValues.ts
@@ -0,0 +1,77 @@
+import type { ProxyHost } from "src/api/backend";
+import { FORWARD_SCHEME, ICON_TYPE, PHP_VERSION, TERMINAL_AUTH_TYPE, TIME_UNIT } from "src/types/enums";
+
+export interface ProxyHostFormValues extends Omit<Partial<ProxyHost>, "advLimitReqRate" | "advLimitReqBurst"> {
+	advLimitReqRate?: number | string;
+	advLimitReqBurst?: number | string;
+	crowdsecEnabled?: boolean;
+	anubisEnabled?: boolean;
+	anubisRules?: ProxyHost["anubisRules"];
+	gitCredentials?: string;
+	php_override_ini?: string;
+}
+
+export const createProxyHostInitialValues = (data: Partial<ProxyHost> = {}): ProxyHostFormValues => ({
+	// Details tab
+	domainNames: data.domainNames || [],
+	forwardScheme: data.forwardScheme || FORWARD_SCHEME.HTTP,
+	forwardHost: data.forwardHost || "",
+	forwardPort: data.forwardPort || undefined,
+	indexFile: data.indexFile || "",
+	// Terminal Fields
+	terminalHost: data.terminalHost || "",
+	terminalPort: data.terminalPort || 22,
+	terminalUsername: data.terminalUsername || "",
+	terminalAuthType: data.terminalAuthType || TERMINAL_AUTH_TYPE.PASSWORD,
+	terminalPassword: data.terminalPassword || "",
+	terminalPrivateKey: data.terminalPrivateKey || "",
+
+	accessListId: data.accessListId || 0,
+	cachingEnabled: data.cachingEnabled || false,
+	disableBuffering: data.disableBuffering || false,
+	blockExploits: data.blockExploits || false,
+	allowWebsocketUpgrade: data.allowWebsocketUpgrade || false,
+	maintenanceOnFailure: data.maintenanceOnFailure || false,
+	// PHP hosting (for scheme=path)
+	phpEnabled: data.phpEnabled || false,
+	phpVersion: data.phpVersion || PHP_VERSION.PHP83,
+	// Locations tab
+	locations: data.locations || [],
+	// SSL tab
+	certificateId: data.certificateId || 0,
+	sslForced: data.sslForced || false,
+	http2Support: data.http2Support || false,
+	hstsEnabled: data.hstsEnabled || false,
+	hstsSubdomains: data.hstsSubdomains || false,
+	// Advanced tab
+	advancedConfig: data.advancedConfig || "",
+	bandwidthLimit: data.bandwidthLimit || "",
+	turboLoader: data.turboLoader || false,
+	advLimitReqRate: data.advLimitReqRate || undefined,
+	advLimitReqUnit: data.advLimitReqUnit || TIME_UNIT.SECONDS,
+	advLimitReqBurst: data.advLimitReqBurst || undefined,
+	forwardQuery: data.forwardQuery || "",
+	maintenanceActive: data.maintenanceActive || false,
+	// datetime-local requires format: YYYY-MM-DDTHH:mm:ss (no timezone)
+	// API returns ISO format with 'Z' suffix, so we strip it
+	maintenanceStart: data.maintenanceStart ? data.maintenanceStart.replace("Z", "").split(".")[0] : "",
+	maintenanceEnd: data.maintenanceEnd ? data.maintenanceEnd.replace("Z", "").split(".")[0] : "",
+	maintenanceReason: data.maintenanceReason || "",
+	// Git Sync
+	gitRepoUrl: data.gitRepoUrl || "",
+	gitBranch: data.gitBranch || "main",
+	gitSyncEnabled: data.gitSyncEnabled || false,
+	gitPollInterval: data.gitPollInterval || 60,
+	gitPollUnit: data.gitPollUnit || TIME_UNIT.MINUTES,
+	gitCredentials: "", // Do not fill credentials for security
+	// Service Icon
+	iconType: data.iconType || ICON_TYPE.AUTO,
+	iconUrl: data.iconUrl || "",
+	// CrowdSec
+	crowdsecEnabled: data.securityCrowdsec || false,
+	// Anubis
+	anubisEnabled: data.anubisEnabled || false,
+	anubisRules: data.anubisRules || [],
+	// Note
+	note: data.note || "",
+});
